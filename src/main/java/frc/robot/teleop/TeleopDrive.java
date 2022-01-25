098303672c4f769c3568b3d1601401673f0e7522
yanlar.23 <yanlar.23@robcol.k12.tr>
2022-01-25 10:33:51 +0300

    teleop drive

--- src/main/java/frc/robot/teleop/TeleopDrive.java
+++ src/main/java/frc/robot/teleop/TeleopDrive.java
@@ -5,11 +5,13 @@
 package frc.robot.teleop;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
 
 public class TeleopDrive extends CommandBase {
   /** Creates a new TeleopDrive. */
   public TeleopDrive() {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.drive);
   }
 
   // Called when the command is initially scheduled.
@@ -18,11 +20,15 @@ public class TeleopDrive extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    RobotContainer.drive.arcadeDrive(RobotContainer.controller.getDriveSpeed(), RobotContainer.controller.getDriveRotation());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    RobotContainer.drive.arcadeDrive(0.0, 0.0);
+  }
 
   // Returns true when the command should end.
   @Override
